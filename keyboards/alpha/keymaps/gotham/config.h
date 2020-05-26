296383b2def7c198420fae54974c5843b65e6538
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-05-26 15:27:48 -0700

    add custom keymap for alpha28

---
+++ keyboards/alpha/keymaps/gotham/config.h
@@ -0,0 +1,5 @@
+#ifdef RGB_DI_PIN
+#define RGBLED_NUM 10
+#endif
+
+#define IGNORE_MOD_TAP_INTERRUPT
---
+++ keyboards/alpha/keymaps/gotham/rules.mk
@@ -0,0 +1,6 @@
+BOOTLOADER = atmel-dfu
+
+BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no	# Mouse keys
+NKRO_ENABLE = no		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+RGBLIGHT_ENABLE = yes
