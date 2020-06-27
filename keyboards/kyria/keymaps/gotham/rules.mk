52c4d6baaba64ac942fef7c5ae0011c4c887605b
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-06-26 22:39:19 -0700

    Get custom transport working

--- keyboards/kyria/keymaps/gotham/rules.mk
+++ keyboards/kyria/keymaps/gotham/rules.mk
@@ -1,9 +1,30 @@
-CONSOLE_ENABLE     = yes  # Console for debug
+MOUSEKEY_ENABLE    = no   # Mouse keys
 ENCODER_ENABLE     = yes  # ENables the use of one or more encoders
 RGBLIGHT_ENABLE    = yes  # Enable keyboard RGB underglow
-MOUSEKEY_ENABLE    = no   # Mouse keys
 OLED_DRIVER_ENABLE = yes  # Enables the use of OLED displays
 THUMBSTICK_ENABLE  = yes  # Enables analog thumbstick code
+
+SPLIT_TRANSPORT = custom
+
+ifeq ($(strip $(SPLIT_TRANSPORT)), custom)
+	OPT_DEFS += -DCUSTOM_TRANSPORT
+	QUANTUM_SRC += transport_custom.c
+	# Functions added via QUANTUM_LIB_SRC are only included in the final binary if they're called.
+	# Unused functions are pruned away, which is why we can add multiple drivers here without bloat.
+	ifeq ($(PLATFORM),AVR)
+		QUANTUM_LIB_SRC += i2c_master.c \
+							i2c_slave.c
+	endif
+
+	SERIAL_DRIVER ?= bitbang
+	ifeq ($(strip $(SERIAL_DRIVER)), bitbang)
+		QUANTUM_LIB_SRC += serial.c
+	else
+		QUANTUM_LIB_SRC += serial_$(strip $(SERIAL_DRIVER)).c
+	endif
+endif
+
+
 
 ifeq ($(strip $(ENCODER_ENABLE)), yes)
 	SRC += encoder_utils.c
