f7e817da28985de3d5d53706a500aaf91a61fb11
Gautham Yerroju <gautham.yerroju@gmail.com>
2020-07-12 21:50:44 -0700

    Change starfield behavior

--- keyboards/kyria/keymaps/gotham/oled_animations/starfield.h
+++ keyboards/kyria/keymaps/gotham/oled_animations/starfield.h
@@ -2,25 +2,27 @@
 #pragma once
 #include "common.h"
 
-#define MAX_STARS 16
-#define Z_FACTOR 1.22
+#define MAX_STARS 24
+#define ZOOM_SPEED 16
 #define SPAWN_RANGE 16
 #define SPAWN_DELAY 333
-#define STARFIELD_FPS 18 // More than 20 and pixels get stuck in the bottom part
-#define CENTER_JUMP_TIMEOUT 1000
+#define STARFIELD_FPS 20 // More than 20 and pixels get stuck in the bottom part
 
 #define UPDATE_DELAY (uint8_t)(1000.0 / STARFIELD_FPS)
 
 void oled_init_starfield(void);
 
-int16_t get_star_x(uint8_t index);
-int16_t get_star_y(uint8_t index);
+uint8_t get_star_x(uint8_t index);
+uint8_t get_star_y(uint8_t index);
 
 void spawn_star(void);
 void update_star(uint8_t index);
+void update_stars(void);
+void draw_stars(void);
+void erase_stars(void);
 
 #ifdef OLED_ANIM_STARFIELD_WANDER
-void update_center(void);
+void set_starfield_center(void);
 #endif
 
 void render_starfield(void);
